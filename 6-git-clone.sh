6ad5c3ac140de3f0ef71f1c10d5200cb7b834205
Kumar Shreyansh <67011742+kumarshreyansh@users.noreply.github.com>
2021-01-25 17:42:02 +0530

    Delete 6-git-clone.sh

--- 6-git-clone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /var/www/
-sudo git clone https://github.com/snipe/snipe-it
-cd /var/www/snipe-it
-sudo cp .env.example .env
-#sudo systemctl restart mysql.service
-#sudo service mysql status
-#sudo cp /home/kumar/.env.sample .env
